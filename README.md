81beff0b2b961e6985985b1caab4cff9f621d29b
zihao_wang <1551902041@qq.com>
2024-03-14 00:11:47 +0800

    login页面表单

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1.20240312搭建好环境
 2.引入antd https://www.bilibili.com/video/BV1i4411N7Qc?p=9&vd_source=4a8608b1bc7490b459ed9563cfccaf28 1分23秒
 3.引入路由
+4.login页面 https://www.bilibili.com/video/BV1i4411N7Qc?p=11&vd_source=4a8608b1bc7490b459ed9563cfccaf28 13分 问题：没引入到less文件
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-// 根组件
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+// 根组件
 
 export default class App extends Component{
     render() {
-        return <div>
-            App
-        </div>
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/login' Component={Login}></Route>
+                    <Route path='/' Component={Admin}></Route>
+                </Routes>
+            </BrowserRouter>
+        )
     }
 }
